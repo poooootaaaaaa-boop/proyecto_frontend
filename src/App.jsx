69e0fffffe24a6a5290529c3b2ff04cf6f418aa0
poooootaaaaaa-boop <poooootaaaaaa@gmail.com>
2026-02-27 14:00:51 -0600

    cambios app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import FarmaciaRoutes from "./routes/FarmaciaRoutes";
+import MedicosRoute from "./routes/MedicosRoutes"; // Esto es lo que trajo el equipo
 import Dashboard from "./pages/Dashboard";
 import Appointments from "./pages/Appointments";
 import Treatments from "./pages/Treatments";
@@ -7,40 +8,42 @@ import NewAppointment from "./components/appointments/NewAppointment";
 import ConfirmAppointment from "./components/appointments/ConfirmAppointment";
 import { AppointmentProvider } from "./components/appointments/AppointmentContext";
 
+// Tus rutas y componentes del stash
+import Layout_Medicos from "./pages/Medicos/Layout_Medicos";
+import Dashboard_medicos from "./pages/Medicos/Dashboard_medicos";
+import Lista_paciente from "./pages/Medicos/lista_paciente";
+import Recetas_medicas from "./pages/Medicos/recetas_medicas";
+import HistorialMedico from "./components/historiapaciente/HistorialMedico";
 import Layout from "./components/layout/Layout";
-export default function App() {
 
+export default function App() {
   return (
     <AppointmentProvider>
-    <Routes>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
 
+        {/* Tus rutas nuevas */}
+        <Route path="/angelito-cachondo" element={<Layout_Medicos />} />
+        <Route path="/recetas_medicas" element={<Recetas_medicas />} />
+        <Route
+          path="/historial_medico"
+          element={
+            <Layout>
+              <HistorialMedico />
+            </Layout>
+          }
+        />
+        <Route path="/lista_paciente" element={<Lista_paciente />} />
+        <Route path="/Dashboard_medicos" element={<Dashboard_medicos />} />
 
-      <Route path="/" element={<Dashboard />} />
-
-
-
-
-
-      <Route
-  path="/historial_medico"
-  element={
-    <Layout>
-      <HistorialMedico />
-    </Layout>
-  }
-/>
-
-      <Route path="/citas" element={<Appointments />} />
-      <Route path="/nueva-cita" element={<NewAppointment />} />
-      <Route path="/confirmar-cita" element={<ConfirmAppointment />} />
-
-
-
-      <Route path="/tratamientos" element={<Treatments />} />
-      <Route path="/farmacia/*" element={<FarmaciaRoutes/>} />
-      <Route path="/Medicos/*" element={<MedicosRoute/>} />
-    </Routes>
+        {/* Rutas generales y del equipo */}
+        <Route path="/citas" element={<Appointments />} />
+        <Route path="/nueva-cita" element={<NewAppointment />} />
+        <Route path="/confirmar-cita" element={<ConfirmAppointment />} />
+        <Route path="/tratamientos" element={<Treatments />} />
+        <Route path="/farmacia/*" element={<FarmaciaRoutes />} />
+        <Route path="/Medicos/*" element={<MedicosRoute />} />
+      </Routes>
     </AppointmentProvider>
   );
-
 }
