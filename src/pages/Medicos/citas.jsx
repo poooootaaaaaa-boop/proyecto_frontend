7ec582183f2b4293b7cab12849be0175077be9d5
poooootaaaaaa-boop <poooootaaaaaa@gmail.com>
2026-03-31 16:15:33 -0600

    Citas en el modulo del Doctor

--- src/pages/Medicos/citas.jsx
+++ src/pages/Medicos/citas.jsx
@@ -9,13 +9,14 @@ import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import Button from 'react-bootstrap/Button';
-import { useState } from "react";
+import { useState ,useEffect } from "react";
 import { Link } from "react-router-dom";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
 import Mensaje from "./mensaje";
+import dayjs from "dayjs";
 import "./calendar.css";
 
 function citas({ data, setData }){
@@ -31,19 +32,68 @@ function citas({ data, setData }){
         const[apellidoM, setApellidoM]=useState("");
         const[fechaCita, setFechaCita]=useState(null);
         const[motivoCita, setMotivoCita]=useState("");
+        const [citasDoctor, setCitasDoctor] = useState([]);
         const [mostrarMensaje,setMostrarMensaje]=useState(false);
-        
-        const FinalizarCita = () => {
-            const nuevaCita = { nombre,apellidoP, apellidoM, fechaCita:fechaCita?.format("YYYY-MM-DD HH:mm"), tipoCita, motivoCita};
-            console.log("Cita creada:", nuevaCita);
+        const [pacientes, setPacientes] = useState([]);
+const [pacienteSeleccionado, setPacienteSeleccionado] = useState("");
+        useEffect(() => {
+    const doctor_id = 1; //  cámbialo por el real (login)
 
-            if (typeof setData === "function") {
-            setData(prev => [...prev, nuevaCita]);
-             setMostrarMensaje(true); 
-            } else {
-            console.error("setData no es una función", setData);
-            }
-        }
+    fetch(`http://localhost:8000/api/pacientes-doctor/${doctor_id}`)
+        .then(res => res.json())
+        .then(data => setPacientes(data))
+        .catch(err => console.error(err));
+}, []);
+
+useEffect(() => {
+  const doctor_id = 1;
+
+  fetch(`http://localhost:8000/api/citas-doctor/${doctor_id}`)
+    .then(res => res.json())
+    .then(data => setCitasDoctor(data))
+    .catch(err => console.error(err));
+}, []);
+        
+
+const esHoraOcupada = (fecha) => {
+  return citasDoctor.some(c =>
+    dayjs(c.fecha_fin).format("YYYY-MM-DD HH:mm") ===
+    dayjs(fecha).format("YYYY-MM-DD HH:mm")
+  );
+};
+
+const FinalizarCita = async () => {
+
+    const doctor_id = 1; // luego lo haces dinámico
+
+    const nuevaCita = {
+        doctor_id,
+        paciente_id: pacienteSeleccionado,
+        fecha_fin: fechaCita?.format("YYYY-MM-DD HH:mm:ss"),
+
+        // AQUÍ LA CLAVE
+        motivo: `${tipoCita.toUpperCase()} - ${motivoCita}`
+    };
+
+    try {
+       const res = await fetch("http://localhost:8000/api/citas", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(nuevaCita)
+});
+
+        const data = await res.json();
+        console.log(data);
+
+        setMostrarMensaje(true);
+
+    } catch (error) {
+        console.error("Error al crear cita:", error);
+    }
+};
 
     return(
         <div>
@@ -60,29 +110,31 @@ function citas({ data, setData }){
                     <Typography style={{fontSize: "17px",color: "gray"}}>Complete los datos para agendar una cita médica</Typography>
                     <br />
 
-                        <div className="bg-white p-4 mb-4" style={{borderRadius:"20px",border:"1px solid #ddd",boxShadow:"0 2px 6px rgba(0,0,0,0.05)"}}>
+<div className="bg-white p-4 mb-4" style={{borderRadius:"20px",border:"1px solid #ddd"}}>
 
-                            <h5 style={{fontWeight:"600"}}><PersonIcon style={{fontSize:"25px",marginRight:"5px",color:"blue"}}/>Información Personal</h5>
+    <h5 style={{fontWeight:"600"}}>
+        <PersonIcon style={{color:"blue"}}/> Paciente
+    </h5>
 
-                            <Row className="mt-3">
-                                <Col md={4}>
-                                    <Form.Label>Nombre</Form.Label>
-                                    <Form.Control type="text"placeholder="Ingresa tu nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-                                </Col>
+    <Row className="mt-3">
+        <Col md={12}>
+            <Form.Label>Seleccionar paciente</Form.Label>
+            <Form.Select
+                value={pacienteSeleccionado}
+                onChange={(e) => setPacienteSeleccionado(e.target.value)}
+            >
+                <option value="">Selecciona un paciente</option>
 
-                                <Col md={4}>
-                                    <Form.Label>Apellido Paterno</Form.Label>
-                                    <Form.Control type="text" placeholder="ingresa tu apellido paterno" value={apellidoP} onChange={(e) => setApellidoP(e.target.value)} />
-                                </Col>
+              {pacientes?.map(p => (
+    <option key={p.id} value={p.id}>
+        {p.usuario?.nombre}
+    </option>
+))}
+            </Form.Select>
+        </Col>
+    </Row>
 
-                                <Col md={4}>
-                                    <Form.Label>Apellido Materno</Form.Label>
-                                    <Form.Control type="text" placeholder="ingresa tu apellido materno" value={apellidoM} onChange={(e) => setApellidoM(e.target.value)} />
-                                </Col>
-
-                            </Row>
-
-                        </div>
+</div>
 
                         <div className="bg-white p-4 mb-4" style={{borderRadius:"20px",border:"1px solid #ddd",boxShadow:"0 2px 6px rgba(0,0,0,0.05)"}}>
                         
@@ -98,7 +150,23 @@ function citas({ data, setData }){
                                         <Form.Label>Fecha y hora de la cita</Form.Label>
 
                                             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                                <StaticDateTimePicker orientation="landscape" value={fechaCita} onChange={(newValue) => setFechaCita(newValue)} className="calendar-clean"  slotProps={{actionBar: {actions: []}}}/>
+                                               <StaticDateTimePicker 
+  orientation="landscape" 
+  value={fechaCita} 
+  onChange={(newValue) => {
+
+    if (!newValue) return;
+
+    if (esHoraOcupada(newValue)) {
+      alert("Esta cita ya está ocupada en ese horario. Por favor, elige otro.");
+      return; //  no deja seleccionar
+    }
+
+    setFechaCita(newValue); //  solo si está libre
+  }} 
+  className="calendar-clean"  
+  slotProps={{actionBar: {actions: []}}}
+/>
                                             </LocalizationProvider>
 
                                     </div>
@@ -136,7 +204,13 @@ function citas({ data, setData }){
                                  
                                 <Col md={6}>
                                     <Form.Label>Motivo de la cita</Form.Label>
-                                    <Form.Control type="text" placeholder="Ingresa el motivo de la cita" value={motivoCita} onChange={(e) => setMotivoCita(e.target.value)} />
+                                    <Form.Control 
+  type="text"
+  placeholder="Ingresa el motivo de la cita"
+  value={motivoCita}
+  onChange={(e) => setMotivoCita(e.target.value)}
+  autoFocus
+/>
                                 </Col>
                         
                             </Row>
